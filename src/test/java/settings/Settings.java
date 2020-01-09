f946e61723922bfbce61bd1a44ab8d5ebaff016f
ombogdan <36894763+ombogdan@users.noreply.github.com>
2020-01-09 12:59:49 +0200

    Change test

--- src/test/java/settings/Settings.java
+++ src/test/java/settings/Settings.java
@@ -34,6 +34,6 @@ public class Settings {
 
     @After
     public void quit() {
-        //driver.quit();
+        driver.quit();
     }
 }
--- src/test/java/task/Agrocontrol.java
+++ src/test/java/task/Agrocontrol.java
@@ -1,21 +1,22 @@
 package task;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import settings.Settings;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.MatcherAssert.assertThat;
 
 public class Agrocontrol extends Settings {
     private AgrocontrolPage agPage = new AgrocontrolPage();
@@ -24,6 +25,10 @@ public class Agrocontrol extends Settings {
     public void getReport() throws InterruptedException, IOException {
         Actions move = new Actions(driver);
         ObjectMapper mapper = new ObjectMapper();
+        Gson gson = new Gson();
+        File actualFile = new File("C:\\Users\\omelc\\IdeaProjects\\agroTest\\src\\test\\java\\data\\actual.json");
+        File changedFile = new File("C:\\Users\\omelc\\IdeaProjects\\agroTest\\src\\test\\java\\data\\changed.json");
+        File etalonFile = new File("C:\\Users\\omelc\\IdeaProjects\\agroTest\\src\\test\\java\\data\\etalon.json");
         driver.findElement(agPage.reportPage).click();
         driver.findElement(agPage.reportBy).click();
         driver.findElement(By.xpath("//*[contains(text(), 'По топливу')]")).click();
@@ -38,23 +43,26 @@ public class Agrocontrol extends Settings {
         WebElement slider = driver.findElement(By.className("hsplitter"));
         Action action = move.dragAndDropBy(slider, 0, 10).build();
         action.perform();
-        File actualFile = new File("C:\\Users\\omelc\\IdeaProjects\\agroTest\\src\\test\\java\\data\\actual.json");
         List<WebElement> data = driver.findElements(By.xpath("//*[@id=\"reportResultTemplate\"]/div/div/div/div[3]/div/div[1]/table/tbody/tr/td[2]"));
-        List<String> newData = new ArrayList<>();
+        List<String> actualDataArray = new ArrayList<>();
         for (WebElement p : data) {
-            newData.add(String.valueOf(p.getText()));
+            actualDataArray.add(String.valueOf(p.getText()));
             try {
-                mapper.readTree("C:\\Users\\omelc\\IdeaProjects\\agroTest\\src\\test\\java\\data\\actual.json");
-                mapper.writeValue(actualFile, newData);
+                mapper.writeValue(actualFile, actualDataArray);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-        File etalonFile = new File("C:\\Users\\omelc\\IdeaProjects\\agroTest\\src\\test\\java\\data\\etalon.json");
         BufferedReader etalonBuffered = new BufferedReader(new FileReader(etalonFile));
-        BufferedReader actualBuffered = new BufferedReader(new FileReader(actualFile));
         String etalonLine = etalonBuffered.readLine();
-        String actualLine = actualBuffered.readLine();
-        assertThat(etalonLine, containsString(actualLine));
+        Type etalonlList = new TypeToken<ArrayList<String>>() {}.getType();
+        List<String> etalonDataArray = gson.fromJson(etalonLine, etalonlList);
+        List<String> changedDataArray = new ArrayList<>();
+        for (int j = 0; j < etalonDataArray.size(); j++) {
+            if (!etalonDataArray.get(j).equals(actualDataArray.get(j))) {
+                changedDataArray.add(actualDataArray.get(j));
+                mapper.writeValue(changedFile, changedDataArray);
+            }
+        }
     }
 }
